ea9145cf43c14652a6fe499a3148c0a82e2e8545
Fosssil <dahiyabadshah.fossil@gmail.com>
2024-12-02 18:40:42 +0530

    02-Dec-2024 06:40 PM

--- aws_multi_tier/security_groups.tf
+++ aws_multi_tier/security_groups.tf
@@ -6,3 +6,19 @@ resource "aws_security_group" "wp-sg" {
     Name = "Wordpress Security Group"
   }
 }
+
+## Outbound rule
+resource "aws_vpc_security_group_egress_rule" "WP_allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.wp-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivallent to all ports
+}
+
+## Inbound rule
+resource "aws_vpc_security_group_ingress_rule" "WP_allow_http_ipv4_for_all" {
+  security_group_id = aws_security_group.wp-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = "80"
+}
